11934e55b60b89952a544b794226713b12379972
S P <sagarpatel16@gmail.com>
2020-02-05 19:30:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
 
 // Rendering app to index.html
 ReactDOM.render(<App />, document.getElementById("root"));
